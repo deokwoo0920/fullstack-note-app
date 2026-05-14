a18f5a79ea8c33625db2ab1e81c406f784e1b5f6
Deokwoo <kdw335552@daum.net>
2026-05-14 16:56:10 +0900

    fullstack-note-app frontend .env.production 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080',
   },
-  output: 'standalone'
 }
 
 export default nextConfig
